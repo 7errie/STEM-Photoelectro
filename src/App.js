28c8ecbbcba94b81e6af52820feabeedfae4f180
terrie kun <x_terroristz@yahoo.com>
2022-07-24 01:30:58 +0800

    Changed BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./pages/MainPage";
 
 class App extends Component {
 
   render() {
     return (
-      <Router basename={window.location.pathname || ''}>
-        <Route exact path="/" element={ <Main /> } />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={ <Main /> } />
+          </Routes>
       </Router>
     );
   }
